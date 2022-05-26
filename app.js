457c6010637a319dd5833579ed66ed28c296f863
Pushpendra Pal <94526347+Pushpendra100@users.noreply.github.com>
2022-05-26 23:58:27 +0530

    port corrected

--- app.js
+++ app.js
@@ -61,6 +61,6 @@ user.save((err)=>{
 
 
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server started successfully");
 });
