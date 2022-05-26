eb46f6e75f9830fe769e519d0ad38e1386882fd1
Pushpendra Pal <94526347+Pushpendra100@users.noreply.github.com>
2022-05-27 00:06:31 +0530

    procfile added

--- app.js
+++ app.js
@@ -60,7 +60,10 @@ user.save((err)=>{
 });
 
 
-
-app.listen(process.env.PORT || 3000, function() {
+let port = process.env.PORT;
+if(port == null || port ==""){
+  port = 3000;
+}
+app.listen(port, function() {
   console.log("Server started successfully");
 });
